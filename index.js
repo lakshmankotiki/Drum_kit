36e6d7af4eda9de59a35e0b85f2f207b51fbbfa0
Lakshman Kotiki <lakshman-kotikiskills1996@gmail.com>
2019-12-30 23:24:17 +0530

    completed playing audio files while clicking on buttons

--- index.js
+++ index.js
@@ -1,6 +1,36 @@
 var buttonLength = document.querySelectorAll(".drum").length;
 for (var i = 0; i < buttonLength; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
-        alert("Clicked......");
+        var buttonHTML = this.innerHTML;
+        switch (buttonHTML) {
+            case "w":
+                var audioW = new Audio("sounds/tom-1.mp3");
+                audioW.play();
+                break;
+            case "a":
+                var audioA = new Audio("sounds/tom-2.mp3");
+                audioA.play();
+                break;
+            case "s":
+                var audioS = new Audio("sounds/tom-3.mp3");
+                audioS.play();
+                break;
+            case "d":
+                var audioD = new Audio("sounds/tom-4.mp3");
+                audioD.play();
+                break;
+            case "j":
+                var audioJ = new Audio("sounds/crash.mp3");
+                audioJ.play();
+                break;
+            case "k":
+                var audioK = new Audio("sounds/kick-bass.mp3");
+                audioK.play();
+                break;
+            case "l":
+                var audioL = new Audio("sounds/snare.mp3");
+                audioL.play();
+                break;
+        }
     });
 }
