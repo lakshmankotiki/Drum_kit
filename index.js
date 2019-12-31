1f82094ad6f5223b7fa4bb33cb1c7749817ef624
Lakshman Kotiki <kotiki.lakshman1996@gmail.com>
2019-12-31 17:22:09 +0530

    handled all 7 cases using switch case

--- index.js
+++ index.js
@@ -2,36 +2,6 @@ var buttonLength = document.querySelectorAll(".drum").length;
 for (var i = 0; i < buttonLength; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function() {
         var buttonHTML = this.innerHTML;
-        // switch (buttonHTML) {
-        //     case "w":
-        //         var audioW = new Audio("sounds/tom-1.mp3");
-        //         audioW.play();
-        //         break;
-        //     case "a":
-        //         var audioA = new Audio("sounds/tom-2.mp3");
-        //         audioA.play();
-        //         break;
-        //     case "s":
-        //         var audioS = new Audio("sounds/tom-3.mp3");
-        //         audioS.play();
-        //         break;
-        //     case "d":
-        //         var audioD = new Audio("sounds/tom-4.mp3");
-        //         audioD.play();
-        //         break;
-        //     case "j":
-        //         var audioJ = new Audio("sounds/crash.mp3");
-        //         audioJ.play();
-        //         break;
-        //     case "k":
-        //         var audioK = new Audio("sounds/kick-bass.mp3");
-        //         audioK.play();
-        //         break;
-        //     case "l":
-        //         var audioL = new Audio("sounds/snare.mp3");
-        //         audioL.play();
-        //         break;
-        // }
         var audio = new Audio();
         switch (buttonHTML) {
             case "w":
@@ -40,6 +10,26 @@ for (var i = 0; i < buttonLength; i++) {
                 break;
             case "a":
                 audio.src = "sounds/tom-2.mp3";
+                audio.play();
+                break;
+            case "s":
+                audio.src = "sounds/tom-3.mp3";
+                audio.play();
+                break;
+            case "d":
+                audio.src = "sounds/tom-4.mp3";
+                audio.play();
+                break;
+            case "j":
+                audio.src = "sounds/crash.mp3";
+                audio.play();
+                break;
+            case "k":
+                audio.src = "sounds/kick-bass.mp3";
+                audio.play();
+                break;
+            case "l":
+                audio.src = "sounds/snare.mp3";
                 audio.play();
                 break;
         }
